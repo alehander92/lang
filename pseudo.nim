0e4ffcd5f33ec57e5339943ca5046775b8a34858
Alexander Ivanov <alehander42@gmail.com>
2018-11-21 23:11:51 +0200

    Type system

--- pseudo.nim
+++ pseudo.nim
@@ -27,7 +27,7 @@ type
     Int,
     Call
 
-  Node* = object
+  Node* = ref object
     case kind*: NodeKind:
     of Name, Operator:
       name*: string
@@ -46,17 +46,24 @@ type
   Type* = object
     case kind*: TypeKind:
     of TSimple:
-      name*:    string
+      discard      
     of TConcrete:
       params*:  seq[Type]
+      base*:    seq[Type]
     of TGeneric:
       args*:    seq[string]
-  
-  TypeEnv* = object
+    name*:      string
+
+
+  TypeEnv* = ref object
     previous*: seq[TypeEnv]
     types*:    Table[string, Type]
 
-proc initTypeEnv*: TypeEnv =
+
+proc check*(node: Node, env: TypeEnv)
+proc checkChildren*(nodes: seq[Node], env: TypeEnv)
+
+proc newTypeEnv*: TypeEnv =
   TypeEnv(previous: @[], types: initTable[string, Type]())
 
 proc `[]`*(env: TypeEnv, name: string): Type =
@@ -73,6 +80,7 @@ proc `[]`*(env: TypeEnv, name: string): Type =
 proc `[]=`*(env: TypeEnv, name: string, t: Type) =
   var types = env.types
   types[name] = t
+  env.types = types
 
 
 proc op*(name: string): Node =
@@ -107,7 +115,7 @@ macro program*(children: untyped): untyped =
   for child in children:
     result.add(pseudoNode(child))
 
-
+  echo result.repr
 
 proc pseudoNode(child: NimNode, depth: int = 0): NimNode =
   # echo repeat("  ", depth), child.repr
@@ -139,8 +147,8 @@ proc pseudoNode(child: NimNode, depth: int = 0): NimNode =
 
 
 let code = program:
+  signature("Int", "Int")
   functionDef("fib", args("n")):
-    signature("Int", "Int")
     code:
       ifNode:
         code:
@@ -176,21 +184,171 @@ macro genCase(t: NodeKind, name: untyped, args: varargs[untyped]): untyped =
 
     result.add(nnkOfBranch.newTree(aNode, code))
 
+proc `[]`*(node; index: int): Node =
+  node.children[index]
+
+proc `[]=`*(node; index: int, value: Node): Node =
+  node.children[index] = value
+
+
+
 proc checkName*(node, env) =
   let t = env[node.name]
-  if t.len == 0:
+  if t.name.len == 0:
+    for name, typ in env.types:
+      echo name, " ", typ.name
     echo &"NO {node.name}"
+    quit(1)
   else:
     node.typ = t
 
 proc checkInt*(node, env) =
-  env["Int"]
+  node.typ = env["Int"]
+
+proc checkString*(node, env) =
+  node.typ = env["String"]
+
+proc genType*(node, env): Type =
+  Type(kind: TSimple, name: node.name)
+
+proc checkFunctionDef*(node, env) =
+  var t = Type(kind: TConcrete, name: "Function")
+  for child in node[0].children: # args
+    t.params.add(genType(child, env))
+  node.typ = t
+  let name = node[1].name
+  let existing = env[name]
+  if existing.name.len > 0:
+    echo &"Overriding {name}"
+    quit(1)
+  env[name] = t
+  var childEnv = newTypeEnv()
+  childEnv.previous = @[env]
+  for i, arg in node[2].children:
+    childEnv[arg.name] = t.params[i]
+  check(node[3], childEnv)
+  
+proc checkOperator(node, env) =
+  discard
+
+template expect(t: Type, u: string): untyped =
+  if t.kind != TSimple or t.name != u:
+    echo "not " & $u
+    quit(1)
+
+
+proc checkIfNode(node, env) =
+  check(node[0], env)
+  if node[0].typ.kind != TSimple or node[0].typ.name != "Bool":
+    echo &"no bool"
+    quit(1)
+  check(node[1], env)
+
+proc checkCode(node, env) =
+  checkChildren(node.children, env)
+
+proc checkInfixOp(node, env) =
+  check(node[1], env)
+  check(node[2], env)
+  let l = node[1].typ
+  let r = node[2].typ
+
+  case node[0].name:
+  of "+", "-", "*", "/":
+    expect(l, "Int")
+    expect(r, "Int")
+    node.typ = env["Int"]
+  of "==", "<", ">":
+    expect(l, "Bool")
+    expect(r, "Bool")
+    node.typ = env["Bool"]
+  else:
+    echo &"operator {node[0].name}"
+    quit(1)
+
+proc checkProgram*(node, env) =
+  for i, child in node.children:
+    case child.kind:
+    of FunctionDef:
+      # signature
+      child.children = @[node.children[i - 1]].concat(child.children)
+      checkFunctionDef(child, env)
+    of Signature:
+      discard
+    else:
+      check(child, env)
 
 proc check*(node, env) =
   genCase(NodeKind.low, check, node, env)
 
-var env = initTypeEnv()
-env["Int"] = Type(kind: TSimple, name: "Int")
+proc checkChildren*(nodes: seq[Node]; env) =
+  for node in nodes:
+    check(node, env)
 
+var env = newTypeEnv()
+env["Int"] = Type(kind: TSimple, name: "Int")
+env["Bool"] = Type(kind: )
 check(code, env)
-  
+
+echo code.typ.kind
+
+
+n -> O[n^2]
+Int -> Int
+def example0(n):
+  var result = 0
+  for i in 0 ..< n:
+    for j in 0 ..< n:
+      result += j
+  return result
+
+n -> O[n^2]
+Int -> Int
+def example1(n):
+  var result = 0
+  for i in 0 ..< n:
+    for j in i ..< n:
+      result += j
+  return result
+
+# example0
+# for:  n
+#   for : n 
+#     code: 1
+#   n
+# n * n <=> n^2
+
+# example1
+# for: n
+#   for: n - i
+#     code: 1
+#   n - i
+# sum(0, n, n - i)
+# n - 0 + n - 1 + n - 2 .. + n - (n - 1)
+# n * n - (n - 1) * (n - 2) / 2
+# n^2 - 0.5n^2 - 1.5 * n + 1
+# 0.5n^2 - 1.5*n <=> n^2
+
+# we need symbolic math
+# seems hard to do
+# maybe we can just special case several cases
+
+# lets see
+
+n -> O[n^2]
+Int
+def example2(n):
+  var result = 0
+  for i in 0 ..< n:
+    if i < 1:
+      example0(i)
+
+
+# example2
+# for: n
+#   if: 1
+#     example0: i ^ 2
+# C != n^2
+# we might need to expand all invocations..
+# and on recursion say: unsupported
+
